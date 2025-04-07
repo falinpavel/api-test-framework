129842ece740b7b19bc8f9acb39dfefba46a27da
FalinPavel <falinpavel96@gmail.com>
2025-04-07 21:50:58 +0300

    refactor endpoit GET/object/{id}

---
+++ endpoints/get_single_object/get_single_object.py
@@ -0,0 +1,37 @@
+import allure
+import requests
+
+
+class GetSingleObject:
+    def __init__(self):
+        self.headers = None
+        self.object = None
+        self.total_objects = None
+        self.status_code = None
+
+    def url_for_get_single_object(self, object_id):
+        with allure.step("Sending GET/objects/{object_id} request"):
+            request = requests.get(f"https://api.restful-api.dev/objects/{object_id}")
+        self.status_code = request.status_code
+        self.total_objects = len(request.json())
+        self.object = request.json()
+        self.headers = request.headers
+        return request
+
+    def check_headers(self):
+        with allure.step("Checking content type"):
+            assert self.headers["Content-Type"] == "application/json"
+
+    def check_response_status_code(self):
+        with allure.step("Checking status code"):
+            assert self.status_code == 200
+
+    def check_response_object_contains_all_params(self):
+        with allure.step("Checking response body contains all params"):
+            get_object = self.object
+            assert get_object["id"] == self.object["id"]
+            assert get_object["name"] == self.object["name"]
+            assert get_object["data"]["year"] == self.object["data"]["year"]
+            assert get_object["data"]["price"] == self.object["data"]["price"]
+            assert get_object["data"]["CPU model"] == self.object["data"]["CPU model"]
+            assert get_object["data"]["Hard disk size"] == self.object["data"]["Hard disk size"]
--- tests/test_get_single_object/test_get_single_object.py
+++ tests/test_get_single_object/test_get_single_object.py
@@ -2,39 +2,22 @@ import requests
 import pytest
 import allure
 
+from endpoints.get_single_object.get_single_object import GetSingleObject
+
 
 @pytest.mark.api
 @allure.epic("REST API")
 @allure.feature("REST API. Method GET/objects/{id}")
 @allure.story("Getting single object by id")
 @allure.title("Получение созданного в предусловии обьекта по его идентификатору")
-@allure.description("""
-1. Создание нового обиекта со следующими параметрами:
-{
-	"name": "Apple MacBook Pro 16",
-	"data": {
-		"year": 2019,
-		"price": 1849.99,
-		"CPU model": "Intel Core i9",
-		"Hard disk size": "1 TB"
-	}
-}
-2. Проверка маппинга каждого поля по отвельности
-""")
+@allure.description(
+    """Тест кейс проверяет, что GET/objects/{id}
+    возвращает статус код 200 ОК, обьект
+    соответствует требованиям (см. параметры созданного обьекта в фикстуре)
+    """)
 def test_get_single_object(generate_new_object):
-    with allure.step("Sending GET/objects/{id} request"):
-        response = requests.get(f"https://api.restful-api.dev/objects/{generate_new_object['id']}")
-    with allure.step("Checking status code"):
-        assert response.status_code == 200
-    with allure.step("Checking content type"):
-        assert response.headers["Content-Type"] == "application/json"
-    with allure.step("Checking response body is not empty"):
-        assert response.json()
-    with allure.step("Checking response body contains all params"):
-        get_object = response.json()
-        # проверяем параметры созданного POST запросом в фикстуре обьекта с полученными в GET запросе (проверка удаления обьекта реализована в фикстуре)
-        assert get_object["name"] == generate_new_object["name"]
-        assert get_object["data"]["year"] == generate_new_object["data"]["year"]
-        assert get_object["data"]["price"] == generate_new_object["data"]["price"]
-        assert get_object["data"]["CPU model"] == generate_new_object["data"]["CPU model"]
-        assert get_object["data"]["Hard disk size"] == generate_new_object["data"]["Hard disk size"]
+    request = GetSingleObject()
+    request.url_for_get_single_object(generate_new_object["id"])
+    request.check_response_status_code()
+    request.check_headers()
+    request.check_response_object_contains_all_params()
